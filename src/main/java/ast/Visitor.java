744a88581585f6a258d722db1493db07e9183a3d
Zhou Nicolas <nicolas.zhou@etu.univ-orleans.fr>
2021-03-09 14:09:52 +0100

    ADD: Visitor hérite VisitorGLo et VisitorDec

--- src/main/java/ast/Visitor.java
+++ src/main/java/ast/Visitor.java
@@ -1,6 +1,6 @@
 package ast;
 
-public interface Visitor<T> extends VisitorTyp<T>, VisitorExp<T> {
+public interface Visitor<T> extends VisitorTyp<T>, VisitorExp<T>, VisitorGlo<T>, VisitorDec<T> {
     // TO COMPLETE
     T visit(Program program);
 }
