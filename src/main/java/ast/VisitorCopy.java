656a8feff89d2a9e557da4913b2930f5ae266829
Ladislus <walcak.ladislas@gmail.com>
2021-03-09 15:54:43 +0100

    [ADD] Added VisitorCopy Expression block

--- src/main/java/ast/VisitorCopy.java
+++ src/main/java/ast/VisitorCopy.java
@@ -1,6 +1,98 @@
 package ast;
 
+import java.util.ArrayList;
+
 public class VisitorCopy<Node> implements Visitor<Node>{
+
+
+    // ##########################
+    // #    Block Expression    #
+    // ##########################
+    /**
+     * @author Ladislas WALCAK
+     * @author Thomas QUETIER
+     * @author Corentin HERVOCHON
+     * @author Martin GUERRAUD
+     */
+    @Override
+    public Node visit(ExpPreUnaryOperation operation) {
+        return new ExpPreUnaryOperation(operation.getExpression(), operation.getOperator()).getExpression().accept(this);
+    }
+
+    @Override
+    public Node visit(ExpPostUnaryOperation operation) {
+        return new ExpPostUnaryOperation(operation.getExpression(), operation.getOperator()).getExpression().accept(this);
+    }
+
+    @Override
+    public Node visit(ExpBinaryOperation operation) {
+        ExpBinaryOperation no = new ExpBinaryOperation(operation.getLeft(), operation.getOperator(), operation.getRight());
+        no.getLeft().accept(this);
+        return no.getRight().accept(this);
+    }
+
+    @Override
+    public Node visit(ExpBoolean bool) {
+        return null;
+    }
+
+    @Override
+    public Node visit(ExpCharacter character) {
+        return null;
+    }
+
+    @Override
+    public Node visit(ExpInteger integer) {
+        return null;
+    }
+
+    @Override
+    public Node visit(ExpVariable variable) {
+        return null;
+    }
+
+    @Override
+    public Node visit(ExpRecord record) {
+        return new ExpRecord(record.getRecord(), new String(record.getField())).getRecord().accept(this);
+    }
+
+    @Override
+    public Node visit(ExpArray array) {
+        ExpArray na = new ExpArray(array.getArray(), array.getIndex());
+        na.getArray().accept(this);
+        return na.getIndex().accept(this);
+    }
+
+    @Override
+    public Node visit(ExpEnum enumeration) {
+        ExpEnum ne = new ExpEnum(new ArrayList<>(enumeration.getElements()));
+        Node curr = null;
+        for (Expression exp : ne.getElements()) curr = exp.accept(this);
+        return curr;
+    }
+
+    @Override
+    public Node visit(ExpTuple tuple) {
+        ExpTuple nt = new ExpTuple(tuple.getFirst(), tuple.getSecond());
+        nt.getFirst().accept(this);
+        return nt.getSecond().accept(this);
+    }
+
+    @Override
+    public Node visit(ExpFunctionCall function) {
+        ExpFunctionCall nf = new ExpFunctionCall(new String(function.getFunction()), new ArrayList<>(function.getArgs()));
+        Node curr = null;
+        for (Expression exp : nf.getArgs()) curr = exp.accept(this);
+        return curr;
+    }
+
+    @Override
+    public Node visit(ExpString string) {
+        return null;
+    }
+    // ##############################
+    // #    Fin Block Expression    #
+    // ##############################
 
 
     /**
@@ -31,4 +123,6 @@ public class VisitorCopy<Node> implements Visitor<Node>{
     public Node visit(TypArray typArray) {
         return new TypArray(typArray.getType()).getType().accept(this);
     }
+
+
 }
