850908ee0629f217f04c98a73dd4a49741ce7669
Zhou Nicolas <nicolas.zhou@etu.univ-orleans.fr>
2021-03-09 16:47:30 +0100

    ADD: VisitorDefault -> definition de fonction, definition de type, imports, déclarations globales et programmes

--- src/main/java/ast/VisitorDefault.java
+++ src/main/java/ast/VisitorDefault.java
@@ -1,5 +1,7 @@
 package ast;
 
+import java.util.List;
+
 public class VisitorDefault<T> implements Visitor<T> {
 
     private T defaultValue;
@@ -121,4 +123,84 @@ public class VisitorDefault<T> implements Visitor<T> {
     public T visit(TypArray typArray) {
         return typArray.getType().accept(this);
     }
+
+    // #################################################################################################
+    // # Bloc Definition de fonction, définition de type, imports, déclarations globales et programmes #
+    // #################################################################################################
+
+    /**
+     * @author  Nicolas ZHOU
+     * @author  Marion JURE
+     * @author  Mathis QUERAULT
+     * @author  Tristan LE SAUX
+     */
+
+    /**
+     * Cette methode visite l'arbre d'une fonction de façon arbitraire :
+     * Declaration -> Instruction
+     * @param function
+     * @return La valeur de la dernière instruction visitée.
+     */
+    @Override
+    public T visit(Function function) {
+        for (Declaration declaration: function.getParameters()) {
+            declaration.accept(this);
+        }
+
+        List<Instruction> instructions = function.getInstructions();
+        int size = instructions.size();
+        for (int index = 0; index < size-1; index++) {
+            instructions.get(index).accept(this);
+        }
+        return instructions.get(size-1).accept(this);
+    }
+
+    @Override
+    public T visit(TypeDefinition typeDefinition) {
+        List<Declaration> declarations = typeDefinition.getDeclarations();
+        int size = declarations.size();
+        for (int index = 0; index < size-1; index++) {
+            declarations.get(index).accept(this);
+        }
+        return declarations.get(size-1).accept(this);
+    }
+
+    @Override
+    public T visit(Import imports) {
+        return defaultValue;
+    }
+
+    @Override
+    public T visit(GlobalDeclaration globalDeclaration) {
+        return globalDeclaration.getExpression().get().accept(this);
+    }
+
+    /**
+     * Cette methode visite l'arbre du programme de façon arbitraire :
+     * Imports -> GlobalDecelaration -> TypeDefinition -> Function
+     * @param program
+     * @return La valeur de la dernière fonction visitée
+     */
+    @Override
+    public T visit(Program program){
+        for (Import my_import : program.getImports()){
+            my_import.accept(this);
+        }
+        for (GlobalDeclaration globalDeclaration : program.getGlobalDeclarations()){
+            globalDeclaration.accept(this);
+        }
+        for (TypeDefinition typeDefinition:program.getTypeDefinitions()){
+            typeDefinition.accept(this);
+        }
+        List<Function> functionList = program.getFunctions();
+        int size = functionList.size();
+        for (int index = 0; index < size-1 ; index++) {
+            functionList.get(index).accept(this);
+        }
+        return functionList.get(size-1).accept(this);
+    }
+
+    // #####################################################################################################
+    // # FIN Bloc Definition de fonction, définition de type, imports, déclarations globales et programmes #
+    // #####################################################################################################
 }
