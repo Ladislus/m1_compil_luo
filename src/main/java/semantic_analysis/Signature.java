73683b11a57b78e1b84781d3a04ffe797f2aaf11
Ladislus <walcak.ladislas@gmail.com>
2021-03-17 09:46:54 +0100

    [ADD] Signature & Signatures v1

--- src/main/java/semantic_analysis/Signature.java
+++ src/main/java/semantic_analysis/Signature.java
@@ -1,6 +1,59 @@
 package semantic_analysis;
 
+import ast.EnumPrimitiveType;
+import ast.Position;
+import ast.TypPrimitive;
+import ast.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Signature {
-  //ToDo: to complete see TP6
-  //      and class src/main/java/compiler/Signature.java in W2.zip
+
+    private static final Position placeholderPosition = new Position(-1, -1);
+    public static final TypPrimitive Integer = new TypPrimitive(placeholderPosition, EnumPrimitiveType.INT);
+    public static final TypPrimitive Bool = new TypPrimitive(placeholderPosition, EnumPrimitiveType.BOOL);
+    public static final TypPrimitive Char = new TypPrimitive(placeholderPosition, EnumPrimitiveType.CHAR);
+
+    public final static Signature binaryArithmetic =
+            buildBinary(Integer,
+                        Integer,
+                        Integer);
+    public final static Signature binaryBoolean =
+            buildBinary(Bool,
+                        Bool,
+                        Bool);
+    public final static Signature unaryArithmetic =
+            buildUnary(Integer,
+                       Integer);
+    public final static Signature unaryBoolean =
+            buildUnary(Bool,
+                       Bool);
+    public final static Signature comparison =
+            buildBinary(Integer,
+                        Integer,
+                        Bool);
+
+    public List<Type> argTypes;
+    public Type returnType;
+
+    public Signature(List<Type> argTypes,
+                      Type returnType) {
+        this.argTypes = argTypes;
+        this.returnType = returnType;
+    }
+
+    public static Signature buildBinary(Type t1, Type t2, Type rt) {
+        List<Type> argTypes = new ArrayList<>();
+        argTypes.add(t1);
+        argTypes.add(t2);
+        return new Signature(argTypes, rt);
+    }
+
+    public static Signature buildUnary(Type type,
+                                        Type rt) {
+        List<Type> argTypes = new ArrayList<>();
+        argTypes.add(type);
+        return new Signature(argTypes, rt);
+    }
 }
--- src/main/java/semantic_analysis/Signatures.java
+++ src/main/java/semantic_analysis/Signatures.java
@@ -1,6 +1,38 @@
 package semantic_analysis;
 
+import ast.EnumUnaryOp;
+import ast.EnumBinaryOp;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Signatures {
-  //ToDo: to complete see TP6
-  //      and class src/main/java/compiler/Signature.java in W2.zip
+    public static final Map<EnumBinaryOp, Signature> binary = buildBinary();
+    public static final Map<EnumUnaryOp, Signature> unary = buildUnary();
+
+    private static Map<EnumBinaryOp, Signature> buildBinary() {
+        Map<EnumBinaryOp, Signature> mapping = new HashMap<>();
+        mapping.put(EnumBinaryOp.ADD, Signature.binaryArithmetic);
+        mapping.put(EnumBinaryOp.SUB, Signature.binaryArithmetic);
+        mapping.put(EnumBinaryOp.MUL, Signature.binaryArithmetic);
+        mapping.put(EnumBinaryOp.DIV, Signature.binaryArithmetic);
+        mapping.put(EnumBinaryOp.MOD, Signature.binaryArithmetic);
+        mapping.put(EnumBinaryOp.AND, Signature.binaryBoolean);
+        mapping.put(EnumBinaryOp.OR, Signature.binaryBoolean);
+        mapping.put(EnumBinaryOp.LT, Signature.comparison);
+        mapping.put(EnumBinaryOp.LTE, Signature.comparison);
+        mapping.put(EnumBinaryOp.GT, Signature.comparison);
+        mapping.put(EnumBinaryOp.GTE, Signature.comparison);
+        // EQ and NEQ not there: they'd need a polymorphic signature
+        return mapping;
+    }
+
+    private static Map<EnumUnaryOp, Signature> buildUnary() {
+        Map<EnumUnaryOp, Signature> mapping = new HashMap<>();
+        mapping.put(EnumUnaryOp.DEC, Signature.unaryArithmetic);
+        mapping.put(EnumUnaryOp.INC, Signature.unaryArithmetic);
+        mapping.put(EnumUnaryOp.MINUS, Signature.unaryArithmetic);
+        mapping.put(EnumUnaryOp.NOT, Signature.unaryBoolean);
+        return mapping;
+    }
 }
