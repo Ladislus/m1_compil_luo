ef7a3cee31d199f746435ea0aa5ae799c4496cfa
Ladislus <walcak.ladislas@gmail.com>
2021-03-22 21:48:46 +0100

    [MOD] Refactored try/catch to prevent code repetition

--- src/main/java/semantic_analysis/SymbolTableBuilder.java
+++ src/main/java/semantic_analysis/SymbolTableBuilder.java
@@ -36,14 +36,64 @@ public class SymbolTableBuilder extends ast.VisitorBase<Void> {
         }
     }
 
-    @Override
-    public Void visit(Program program) {
-        super.visit(program);
+    private void insertUserType(TypeDefinition typeDefinition) {
+        try {
+            this.table.insertUserType(typeDefinition);
+        } catch (UserTypeAlreadyExistsException e) {
+            this.errors.add(e.format(typeDefinition.getPosition(), typeDefinition.getName()));
+        }
+    }
+
+    private void insertFunction(Function function) {
+        List<Type> parameters = new ArrayList<>();
+        for (Declaration d : function.getParameters())
+            parameters.add(d.getType());
+        Type returnType = function.getReturn_type().isPresent() ? function.getReturn_type().get() : Signature.Void;
+        try {
+            this.table.insertFunction(function.getName(), new Signature(parameters, returnType));
+        } catch (FunctionSignatureAlreadyExistsException e) {
+            this.errors.add(e.format(function.getPosition(), function.getName()));
+        }
+    }
+
+    private void insertGlobalVariable(Declaration globalDeclaration) {
+        try {
+            this.table.insertGlobalVariable(globalDeclaration.getVariable(), globalDeclaration.getType());
+        } catch (GlobalVariableAlreadyExistsException e) {
+            this.errors.add(e.format(globalDeclaration.getPosition(), globalDeclaration.getVariable()));
+        }
+    }
+
+    private void insertVariable(Declaration declaration) {
+        try {
+            this.table.insertVariable(declaration.getVariable(), current, declaration.getType());
+        } catch (VariableAlreadyExistsInScopeException e) {
+            this.errors.add(e.format(declaration.getPosition(), declaration.getVariable()));
+        } catch (BlockDosentExistsException e) {
+            this.errors.add(e.format(declaration.getPosition()));
+        }
+    }
+
+    private void varLookup(ExpVariable variable) {
+        try {
+            this.table.varLookup(variable.getVariable(), this.blocks.copy());
+        } catch (VariableDosentExistsException e) {
+            this.errors.add(e.format(variable.getPosition(), variable.getVariable()));
+        }
+    }
+
+    private void verifyCalls() {
         for (String call : this.functionCalls.keySet()) {
             if (this.table.funcLookup(call).isEmpty()) {
                 this.errors.add(this.functionCalls.get(call) + " Function " + call + " called, but not defined");
             }
         }
+    }
+
+    @Override
+    public Void visit(Program program) {
+        super.visit(program);
+        this.verifyCalls();
         return null;
     }
 
@@ -59,55 +109,28 @@ public class SymbolTableBuilder extends ast.VisitorBase<Void> {
     public Void visit(Function function) {
         this.current = function.getBody();
         this.table.insertBlock(function.getBody());
-        List<Type> parameters = new ArrayList<>();
-        for (Declaration d : function.getParameters())
-            parameters.add(d.getType());
-        Type returnType = function.getReturn_type().isPresent() ? function.getReturn_type().get() : Signature.Void;
-        try {
-            this.table.insertFunction(function.getName(), new Signature(parameters, returnType));
-        } catch (FunctionSignatureAlreadyExistsException e) {
-            this.errors.add(e.format(function.getPosition(), function.getName()));
-        }
+        this.insertFunction(function);
         return super.visit(function);
     }
 
     @Override
     public Void visit(GlobalDeclaration globalDeclaration) {
-        try {
-            this.table.insertGlobalVariable(globalDeclaration.getVariable(), globalDeclaration.getType());
-        } catch (GlobalVariableAlreadyExistsException e) {
-            this.errors.add(e.format(globalDeclaration.getPosition(), globalDeclaration.getVariable()));
-        }
+        this.insertGlobalVariable(globalDeclaration);
         return super.visit(globalDeclaration);
     }
 
     @Override
     public Void visit(Declaration declaration) {
         if (Objects.isNull(current))
-            try {
-                this.table.insertGlobalVariable(declaration.getVariable(), declaration.getType());
-            } catch (GlobalVariableAlreadyExistsException e) {
-                this.errors.add(e.format(declaration.getPosition(), declaration.getVariable()));
-            }
-        else {
-            try {
-                this.table.insertVariable(declaration.getVariable(), current, declaration.getType());
-            } catch (VariableAlreadyExistsInScopeException e) {
-                this.errors.add(e.format(declaration.getPosition(), declaration.getVariable()));
-            } catch (BlockDosentExistsException e) {
-                this.errors.add(e.format(declaration.getPosition()));
-            }
-        }
+            this.insertGlobalVariable(declaration);
+        else
+            this.insertVariable(declaration);
         return super.visit(declaration);
     }
 
     @Override
     public Void visit(ExpVariable variable) {
-        try {
-            this.table.varLookup(variable.getVariable(), this.blocks.copy());
-        } catch (VariableDosentExistsException e) {
-            this.errors.add(e.format(variable.getPosition(), variable.getVariable()));
-        }
+        this.varLookup(variable);
         return super.visit(variable);
     }
 
@@ -119,11 +142,7 @@ public class SymbolTableBuilder extends ast.VisitorBase<Void> {
 
     @Override
     public Void visit(TypeDefinition typeDefinition) {
-        try {
-            this.table.insertUserType(typeDefinition);
-        } catch (UserTypeAlreadyExistsException e) {
-            this.errors.add(e.format(typeDefinition.getPosition(), typeDefinition.getName()));
-        }
+        this.insertUserType(typeDefinition);
         return super.visit(typeDefinition);
     }
 }
