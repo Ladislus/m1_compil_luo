5fee8f273728b3258aeeae6fe00024e67dc778c4
Frederic Loulergue <frederic@loulergue.eu>
2021-04-13 14:13:03 +0200

    [FIX] La classe Pair est maintenant munie de méthodes equals et hashCode qui sont nécessaires à l'utilisation d'objets de cette classe en tant que clés de table d'associations (Map).

--- src/main/java/support/Pair.java
+++ src/main/java/support/Pair.java
@@ -1,5 +1,7 @@
 package support;
 
+import java.util.Objects;
+
 public class Pair<T1, T2> {
   private T1 fst;
   private T2 snd;
@@ -15,5 +17,26 @@ public class Pair<T1, T2> {
   public Pair(T1 fst, T2 snd) {
     this.fst = fst;
     this.snd = snd;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Pair<?, ?> pair = (Pair<?, ?>) o;
+    return Objects.equals(fst, pair.fst) && Objects.equals(snd, pair.snd);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(fst, snd);
+  }
+
+  @Override
+  public String toString() {
+    return "Pair{" +
+      "fst=" + fst +
+      ", snd=" + snd +
+      '}';
   }
 }
