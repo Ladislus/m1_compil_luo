82fa1e49756ecf80fe44695ad09fdb1257a4c18b
Ladislus <walcak.ladislas@gmail.com>
2021-04-29 16:06:57 +0200

    [ADD] Added expression.binary

--- src/main/java/mips/Expression.java
+++ src/main/java/mips/Expression.java
@@ -3,9 +3,12 @@ package mips;
 import ir.Register;
 import ir.RegisterOffset;
 import ir.Type;
+import ir.com.Label;
 import ir.expr.Byte;
 import ir.expr.*;
 import support.ListTools;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -91,8 +94,91 @@ public class Expression implements Visitor<List<String>> {
 
     @Override
     public List<String> visit(Binary exp) {
-        //ToDo: à compléter pour le TP9
-        return null; // Ne pas retourner null!
+        exp.getLeft().accept(this);
+        List<String> asmCode = new ArrayList<>(Asm.pop("$t0"));
+        exp.getRight().accept(this);
+        asmCode.addAll(Asm.pop("$t1"));
+        switch (exp.getOp()) {
+            case ADD -> asmCode.add(Asm.command("add $t2, $t0, $t1"));
+            case SUB -> asmCode.add(Asm.command("sub $t2, $t0, $t1"));
+            case MUL -> {
+                asmCode.add(Asm.command("mult $t0, $t1"));
+                // Si mfhi > 0, alors le résultat fait plus de 32 bits, donc edge case
+                asmCode.add(Asm.command("mflo $t2"));
+            }
+            case DIV -> {
+                asmCode.add(Asm.command("div $t0, $t1"));
+                // Si mfhi contient le reste, mais division entière
+                asmCode.add(Asm.command("mflo $t2"));
+            }
+            case MOD -> {
+                asmCode.add(Asm.command("div $t0, $t1"));
+                // mfhi contient le reste
+                asmCode.add(Asm.command("mfhi $t2"));
+            }
+            case EQ -> {
+                Label ok = Label.fresh();
+                Label exit = Label.fresh();
+                asmCode.add(Asm.command("beq $t0, $t1, " + ok));
+                asmCode.add(Asm.command("li $t2, 0"));
+                asmCode.add(Asm.command("j " + exit));
+                asmCode.add(Asm.label(ok.toString()));
+                asmCode.add(Asm.command("li $t2, 1"));
+            }
+            case DIFF -> {
+                Label ok = Label.fresh();
+                Label exit = Label.fresh();
+                asmCode.add(Asm.command("beq $t0, $t1, " + ok));
+                asmCode.add(Asm.command("li $t2, 1"));
+                asmCode.add(Asm.command("j " + exit));
+                asmCode.add(Asm.label(ok.toString()));
+                asmCode.add(Asm.command("li $t2, 0"));
+            }
+            case GT -> {
+                Label ok = Label.fresh();
+                Label exit = Label.fresh();
+                asmCode.add(Asm.command("sub $t3, $t0, $t1"));
+                asmCode.add(Asm.command("bgtz " + ok));
+                asmCode.add(Asm.command("li $t2, 0"));
+                asmCode.add(Asm.command("j " + exit));
+                asmCode.add(Asm.label(ok.toString()));
+                asmCode.add(Asm.command("li $t2, 1"));
+            }
+            case GTE -> {
+                Label ok = Label.fresh();
+                Label exit = Label.fresh();
+                asmCode.add(Asm.command("sub $t3, $t0, $t1"));
+                asmCode.add(Asm.command("bgez " + ok));
+                asmCode.add(Asm.command("li $t2, 0"));
+                asmCode.add(Asm.command("j " + exit));
+                asmCode.add(Asm.label(ok.toString()));
+                asmCode.add(Asm.command("li $t2, 1"));
+            }
+            case LT -> {
+                Label ok = Label.fresh();
+                Label exit = Label.fresh();
+                asmCode.add(Asm.command("sub $t3, $t1, $t0"));
+                asmCode.add(Asm.command("bgtz " + ok));
+                asmCode.add(Asm.command("li $t2, 0"));
+                asmCode.add(Asm.command("j " + exit));
+                asmCode.add(Asm.label(ok.toString()));
+                asmCode.add(Asm.command("li $t2, 1"));
+            }
+            case LTE -> {
+                Label ok = Label.fresh();
+                Label exit = Label.fresh();
+                asmCode.add(Asm.command("sub $t3, $t1, $t0"));
+                asmCode.add(Asm.command("bgez " + ok));
+                asmCode.add(Asm.command("li $t2, 0"));
+                asmCode.add(Asm.command("j " + exit));
+                asmCode.add(Asm.label(ok.toString()));
+                asmCode.add(Asm.command("li $t2, 1"));
+            }
+            case AND -> asmCode.add(Asm.command("and $t2, $t0, $t1"));
+            case OR -> asmCode.add(Asm.command("or $t2, $t0, $t1"));
+        }
+        asmCode.addAll(Asm.push("$t2"));
+        return asmCode;
     }
 
     @Override
